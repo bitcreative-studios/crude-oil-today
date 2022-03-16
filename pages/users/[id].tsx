e26c8ff0faef94196bf9974fdea78584d6fa6706
George Bockari <bitwhys@gmail.com>
2022-03-15 22:18:07 -0400

    refactor(user): fix broken imports

--- pages/users/[id].tsx
+++ pages/users/[id].tsx
@@ -2,8 +2,8 @@ import { GetStaticProps, GetStaticPaths } from 'next'
 
 import { User } from '../../interfaces'
 import { sampleUserData } from '../../utils/sample-data'
-import Layout from '../../components/Layout'
-import ListDetail from '../../components/ListDetail'
+import Layout from '../../ui/Layout'
+import { ListDetail } from '../../ui/List'
 
 type Props = {
   item?: User
@@ -36,7 +36,7 @@ export default StaticPropsDetail
 
 export const getStaticPaths: GetStaticPaths = async () => {
   // Get the paths we want to pre-render based on users
-  const paths = sampleUserData.map((user) => ({
+  const paths = sampleUserData.map(user => ({
     params: { id: user.id.toString() },
   }))
 
@@ -51,7 +51,7 @@ export const getStaticPaths: GetStaticPaths = async () => {
 export const getStaticProps: GetStaticProps = async ({ params }) => {
   try {
     const id = params?.id
-    const item = sampleUserData.find((data) => data.id === Number(id))
+    const item = sampleUserData.find(data => data.id === Number(id))
     // By returning { props: item }, the StaticPropsDetail component
     // will receive `item` as a prop at build time
     return { props: { item } }
--- ui/List/index.ts
+++ ui/List/index.ts
@@ -1,2 +1,3 @@
 export { default as ListItem } from './ListItem'
+export { default as ListDetail } from './ListDetail'
 export { default } from './List'
